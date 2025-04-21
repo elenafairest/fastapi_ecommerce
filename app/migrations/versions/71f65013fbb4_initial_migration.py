e7ebb9b8bd1f70a3282dada18fad701cd6634dd5
elena <elenafairest@gmail.com>
2025-04-21 21:36:35 +0400

    Add JWT

--- app/migrations/versions/71f65013fbb4_initial_migration.py
+++ app/migrations/versions/71f65013fbb4_initial_migration.py
@@ -1,8 +1,8 @@
 """Initial migration
 
-Revision ID: db28592e2d1a
+Revision ID: 71f65013fbb4
 Revises: 
-Create Date: 2025-04-19 23:23:53.556682
+Create Date: 2025-04-21 20:52:43.063540
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'db28592e2d1a'
+revision: str = '71f65013fbb4'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -32,6 +32,22 @@ def upgrade() -> None:
     )
     op.create_index(op.f('ix_categories_id'), 'categories', ['id'], unique=False)
     op.create_index(op.f('ix_categories_slug'), 'categories', ['slug'], unique=True)
+    op.create_table('users',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('first_name', sa.String(), nullable=True),
+    sa.Column('last_name', sa.String(), nullable=True),
+    sa.Column('username', sa.String(), nullable=True),
+    sa.Column('email', sa.String(), nullable=True),
+    sa.Column('hashed_password', sa.String(), nullable=True),
+    sa.Column('is_active', sa.Boolean(), nullable=True),
+    sa.Column('is_admin', sa.Boolean(), nullable=True),
+    sa.Column('is_supplier', sa.Boolean(), nullable=True),
+    sa.Column('is_customer', sa.Boolean(), nullable=True),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('email'),
+    sa.UniqueConstraint('username')
+    )
+    op.create_index(op.f('ix_users_id'), 'users', ['id'], unique=False)
     op.create_table('products',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(), nullable=True),
@@ -40,10 +56,12 @@ def upgrade() -> None:
     sa.Column('price', sa.Integer(), nullable=True),
     sa.Column('image_url', sa.String(), nullable=True),
     sa.Column('stock', sa.Integer(), nullable=True),
+    sa.Column('supplier_id', sa.Integer(), nullable=True),
     sa.Column('category_id', sa.Integer(), nullable=True),
     sa.Column('rating', sa.Float(), nullable=True),
     sa.Column('is_active', sa.Boolean(), nullable=True),
     sa.ForeignKeyConstraint(['category_id'], ['categories.id'], ),
+    sa.ForeignKeyConstraint(['supplier_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_products_id'), 'products', ['id'], unique=False)
@@ -57,6 +75,8 @@ def downgrade() -> None:
     op.drop_index(op.f('ix_products_slug'), table_name='products')
     op.drop_index(op.f('ix_products_id'), table_name='products')
     op.drop_table('products')
+    op.drop_index(op.f('ix_users_id'), table_name='users')
+    op.drop_table('users')
     op.drop_index(op.f('ix_categories_slug'), table_name='categories')
     op.drop_index(op.f('ix_categories_id'), table_name='categories')
     op.drop_table('categories')
